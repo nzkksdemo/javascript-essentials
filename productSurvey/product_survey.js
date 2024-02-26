946bad857e9170a5c98a0492109967db391d6186
nzkksdemo <nzkksdemo@gmail.com>
2024-02-25 19:24:07 -0500

    Add additional textarea as per the task

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -15,6 +15,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperience').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -25,6 +26,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = userExperience;
 
     document.getElementById('userInfo').style.display = 'block';
 }
