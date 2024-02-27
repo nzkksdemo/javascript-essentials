5225f260758eef4d627ea1c6cf6c6ef34488d38e
nzkksdemo <nzkksdemo@gmail.com>
2024-02-27 01:22:15 -0500

    Debug code: Add more arithmetic operations and display result

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -6,9 +6,14 @@ function performOperation() {
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
     // Perform the operation
-        let result = multiply(num1, num2);
+        let resultFromMultiply = multiply(num1, num2);
+        let resultFromAddition = addition(num1, num2);
+        let resultFromSubstraction = substraction(num1, num2);
+        let resultFromDivision = division(num1, num2);
+
+        let combinedResult = `<b>The results are:</b> <p>From multiplication: ${resultFromMultiply}</p><p>From addition: ${resultFromAddition}</p><p>From substraction: ${resultFromSubstraction}</p><p>From division: ${resultFromDivision}</p>`;
         // Display the result
-        displayResult(result);
+        displayResult(combinedResult);
     } else {
         displayResult('Please enter valid numbers');
     }
@@ -21,8 +26,29 @@ function multiply(a, b) {
     return a * b;
 }
 
+function addition(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // add the numbers
+    return a + b;
+}
+
+function substraction(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // substract the number "b" from "a"
+    return a - b;
+}
+
+function division(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // divide the number "a" by number "b"
+    return a / b;
+}
+
 function displayResult(result) {
     // Display the result in the paragraph element
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+    resultElement.innerHTML = `<div>${result}</div>`;
 }
